1ae8cc78a22b47ff5ef1107aac831ca4c5bae555
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-03-04 15:38:38 -0500

    Add counting button fn globally + counting button fn locally

--- src/todo/views.cljs
+++ src/todo/views.cljs
@@ -1,15 +1,33 @@
 (ns todo.views
   (:require
-   [re-frame.core :refer [subscribe]]
-   [todo.router :refer [url-for]]))
+   [reagent.core :as reagent]
+   [re-frame.core :refer [dispatch subscribe]]
+   [todo.router :refer [url-for]]
+   [todo.subs :as subs]))
+
+#_(defn counting-button [txt]
+  (let [state (reagent/atom 0)] ;; button counter with local state
+    (fn [txt]
+      [:button.green
+        {:on-click #(swap! state inc)}
+        (str txt " " @state)])))
 
 (defn home-page
   []
-  [:div
-   [:h2 "Hello world"]
-   [:a
-    {:href (url-for :todos)}
-    "Click me to go to todos page"]])
+  (let [button-count (subscribe [::subs/button-info])] ;; button counter with global state
+    (fn []
+      [:div
+       [:h2 "Button Counter"]
+       #_[counting-button "click me"]
+       [:button
+        {:on-click (fn [e]
+                     (.preventDefault e)
+                     (dispatch [:increment-button]))}
+        "click me " @button-count]
+       [:div
+        [:a
+         {:href (url-for :todos)}
+         "Click me"]]])))
 
 (defn todos-page
   []
