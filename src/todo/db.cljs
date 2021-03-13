9cacb51a35a00bc29bbc804f5a5f37d8eabb07c2
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-03-12 23:38:21 -0500

    Add :done keyword to app-db

--- src/todo/db.cljs
+++ src/todo/db.cljs
@@ -6,10 +6,13 @@
    :user-info ""
    :todos [{:id "06512da8-43e0-489f-8ed9-9204ddc0a6b5"
             :name "car"
-            :body "get gas"}
+            :body "get gas"
+            :done false}
            {:id "06512da8-43e0-489f-8ed9-9204ddc0a6b6"
             :name "groceries"
-            :body "bananas"}
+            :body "bananas"
+            :done false}
            {:id "06512da8-43e0-489f-8ed9-9204ddc0a6b7"
             :name "car wash"
-            :body "manual"}]})
+            :body "manual"
+            :done false}]})
