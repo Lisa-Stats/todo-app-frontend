1825e6df3a0ee2c80cb51eb933a8b78a57623033
Lisa-Stats <lisa.stathopulos@gmail.com>
2021-04-01 00:31:42 -0400

    Add localStorage key + put todos into localStorage + use reg-cofx to
    register coeffect handler to inject todos to localStorage

--- src/todo/db.cljs
+++ src/todo/db.cljs
@@ -1,5 +1,21 @@
-(ns todo.db)
+(ns todo.db
+  (:require [cljs.reader]
+            [re-frame.core :as r]))
 
 (def app-db
   {:active-page :home
    :button-info 0})
+
+(def ls-key "todos-reframe")
+
+(defn todos->local-store
+  [todos]
+  (.setItem js/localStorage ls-key (str todos)))
+
+(r/reg-cofx
+ :local-store-todos
+ (fn [cofx _]
+   (assoc cofx :local-store-todos
+          (into (sorted-map)
+                (some->> (.getItem js/localStorage ls-key)
+                         (cljs.reader/read-string))))))
